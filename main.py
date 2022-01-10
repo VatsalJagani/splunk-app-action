19b7ccf724dffb75e87dc42311fb9a2100993a61
Vatsal Jagani <vatsaljagani85@gmail.com>
2022-01-10 13:45:00 +0530

    Added action input parameter for app_package_id.

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ app_dir = utils.get_input('app_dir')
 utils.info("app_dir: {}".format(app_dir))
 app_build_name = utils.get_input('app_build_name')
 utils.info("app_build_name: {}".format(app_build_name))
+app_package_id_input = utils.get_input('app_package_id')
+utils.info("app_package_id: {}".format(app_package_id_input))
+
 
 app_package_id = None
 
@@ -30,23 +33,29 @@ def remove_git_folders():
 
 def fetch_app_package_id():
     utils.info("Fetching app package id. from app.conf file.")
+    if app_package_id_input != "NONE":
+        utils.info("Using app package id provided by user - {}".format(app_package_id_input))
+        return app_package_id_input
     try:
         config = configparser.ConfigParser()
         config.read(os.path.join('repodir', app_dir, 'default', 'app.conf'))
         utils.info("app.conf sections: {}".format(config.sections()))
         if 'package' in config and 'id' in config['package']:
+            utils.info("Using app package id found in app.conf - {}".format(config['package']['id']))
             return config['package']['id']
         else:
             utils.error("It is recommended to have id attribute in the app.conf's [package] stanza.")
-            return None
+            utils.info("Using app_build_name as app package id - {}".format(app_build_name))
+            return app_build_name
     except Exception as e:
         utils.error("Unable to fetch the app-package-id from app.conf. {}".format(e))
         utils.error(traceback.format_exc())
+        utils.info("Using app_build_name as app package id - {}".format(app_build_name))
+        return app_build_name
 
 
 
 def generate_app_build():
-    global app_package_id
     utils.info("Generating the app build. app_dir={}, app_package_id={}".format(app_dir, app_package_id))
 
     # utils.list_files(os.getcwd())
@@ -62,10 +71,8 @@ def generate_app_build():
         utils.info("updated cwd={}".format(os.getcwd()))
         # utils.list_files(os.getcwd())
         
-        if app_package_id and app_dir != app_package_id:
+        if app_dir != app_package_id:
             os.system('mv {} {}'.format(app_dir, app_package_id))
-        else:
-            app_package_id = app_dir
         
         os.system("find {} -type f -exec chmod 644 '{{}}' \;".format(app_package_id))
         os.system("find {} -type d -exec chmod 755 '{{}}' \;".format(app_package_id))
