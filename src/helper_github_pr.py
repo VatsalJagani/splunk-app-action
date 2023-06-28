bb16410563f7e073fbb4d2d0996be92462644602
Vatsal Jagani <vatsaljagani85@gmail.com>
2023-06-29 01:34:10 +0530

    Added utilities input and added code to detect the default git branch.

--- src/helper_github_pr.py
+++ src/helper_github_pr.py
@@ -1,5 +1,6 @@
 
 import os
+import subprocess
 import hashlib
 import helper_github_action as utils
 
@@ -15,8 +16,18 @@ def get_file_hash(file_path):
 class GitHubPR:
     def __init__(self, repo_dir) -> None:
         self.REPO_DIR = repo_dir
-        self.main_branch_name = utils.get_input('main_branch_name')
-        utils.info("main_branch_name: {}".format(self.main_branch_name))
+        self.default_branch_name = self._get_default_branch()
+        utils.info("default_branch_name: {}".format(self.default_branch_name))
+
+
+    def _get_default_branch(self):
+        _default_branch_name_input = utils.get_input('default_branch_name')
+        if _default_branch_name_input and _default_branch_name_input!="NONE":
+            return _default_branch_name_input
+
+        output = subprocess.check_output(['git', 'symbolic-ref', 'refs/remotes/origin/HEAD'])
+        branch_name = output.decode('utf-8').strip().split('/')[-1]
+        return branch_name
 
 
     def check_branch_does_not_exist(self, branch_name):
@@ -32,7 +43,7 @@ class GitHubPR:
 
     def _configure_git(self):
         if not os.environ.get("GITHUB_TOKEN"):
-            raise ValueError("Please configure the GitHub Token in GH_TOKEN environment secret for actions in the Repo Settings.")
+            raise ValueError("Please configure the GitHub Token in MY_GITHUB_TOKEN environment secret for actions in the Repo Settings.")
 
         os.system(r'gh auth setup-git')
         os.system(r'git config user.name splunk_app_action')
@@ -40,13 +51,13 @@ class GitHubPR:
         # TODO - Create this GitHub account
 
 
-    def _commit(self, main_branch, new_branch):
+    def _commit(self, new_branch):
         utils.info("Committing the code.")
-        # checkout main branch
-        os.system(r'git checkout {}'.format(main_branch))
+        # checkout default branch
+        os.system(r'git checkout {}'.format(self.default_branch_name))
 
         # Create a new branch
-        os.system(r'git checkout -b {} {}'.format(new_branch, main_branch))
+        os.system(r'git checkout -b {} {}'.format(new_branch, self.default_branch_name))
 
         # Add app changes
         os.system(r'git add -A')
@@ -55,7 +66,7 @@ class GitHubPR:
         os.system(r'git commit -m "{}"'.format(new_branch))
 
 
-    def _pr(self, main_branch, new_branch):
+    def _pr(self, new_branch):
         utils.info("Pushing the code and creating PR.")
         # Push branch on github
         return_value = os.system(r'git push -u origin {}'.format(new_branch))
@@ -64,7 +75,7 @@ class GitHubPR:
             return
 
         # Create PR
-        os.system(r'gh pr create --base {} --head {} --fill'.format(main_branch, new_branch))
+        os.system(r'gh pr create --base {} --head {} --fill'.format(self.default_branch_name, new_branch))
 
 
     def commit_and_pr(self, file_to_generate_hash, local_test=False):
@@ -77,8 +88,8 @@ class GitHubPR:
             if not local_test:
                 if is_any_update:
                     self._configure_git()
-                    self._commit(self.main_branch_name, new_branch)
-                    self._pr(self.main_branch_name, new_branch)
+                    self._commit(new_branch)
+                    self._pr(new_branch)
                 else:
                     utils.info("No changes.")
             else:
