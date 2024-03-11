cae05a88ed33bb89b489714317e73d4294e0190b
Vatsal Jagani <vatsaljagani85@gmail.com>
2024-03-11 14:41:59 +0530

    Second integration test added with some generic code improvement.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -15,9 +15,74 @@ import os
 import sys
 import shutil
 import tarfile
-from main import main
+from contextlib import contextmanager
 
+from main import main
 from helpers.github_action_utils import set_env
+
+
+@contextmanager
+def setup_action_yml_work(test_app_repo,
+                            app_dir=".",
+                            is_generate_build="true",
+                            use_ucc_gen="false",
+                            to_make_permission_changes="false",
+                            is_app_inspect_check="true",
+                            splunkbase_username="NONE",
+                            splunkbase_password="NONE",
+                            app_build_path="NONE",
+                            app_utilities="",
+                            my_github_token="NONE",
+                            current_branch="NONE",
+                            logger_log_files_prefix="NONE",
+                            logger_sourcetype="NONE"):
+
+    app_dir_path = os.path.join(os.path.dirname(__file__), "test_app_repos", test_app_repo)
+    print(f"TestIntegration.setup_action_yml_work -> app_dir_path={app_dir_path}")
+
+    # copy the app module there
+    shutil.copytree(app_dir_path, os.path.join("repodir"))
+
+    # setup inputs
+    os.environ["SPLUNK_app_dir"] = app_dir
+    os.environ["SPLUNK_is_generate_build"] = is_generate_build
+    os.environ["SPLUNK_use_ucc_gen"] = use_ucc_gen
+    os.environ["SPLUNK_to_make_permission_changes"] = to_make_permission_changes
+    os.environ["SPLUNK_is_app_inspect_check"] = is_app_inspect_check
+    os.environ["SPLUNK_splunkbase_username"] = splunkbase_username
+    os.environ["SPLUNK_splunkbase_password"] = splunkbase_password
+    os.environ["SPLUNK_app_build_path"] = app_build_path
+    os.environ["SPLUNK_app_utilities"] = app_utilities
+    os.environ["GITHUB_TOKEN"] = my_github_token
+    os.environ["SPLUNK_current_branch_name"] = current_branch
+    os.environ["SPLUNK_logger_log_files_prefix"] = logger_log_files_prefix
+    os.environ["SPLUNK_logger_sourcetype"] = logger_sourcetype
+
+    try:
+        yield
+    finally:
+        print("TestIntegration Cleanup after each test-case.")
+        try:
+            shutil.rmtree("repodir")
+        except:
+            pass
+
+        try:
+            shutil.rmtree("without_ucc_build")
+        except:
+            pass
+
+        try:
+            shutil.rmtree("with_ucc_build")
+        except:
+            pass
+
+        for filename in os.listdir():
+            if filename.startswith("my_app_"):
+                if os.path.isdir(filename):
+                    shutil.rmtree(filename)
+                else:
+                    os.remove(filename)
 
 
 
@@ -33,67 +98,6 @@ class TestIntegration(unittest.TestCase):
             print(f"Mocked set_env called with args: name={name}, value={value}")
             os.environ[name] = value
         self.mock_set_env.side_effect = mock_set_env
-
-
-    def setup_action_yml_work(self,
-                              test_app_repo,
-                              app_dir=".",
-                              is_generate_build="true",
-                              use_ucc_gen="false",
-                              to_make_permission_changes="false",
-                              is_app_inspect_check="true",
-                              splunkbase_username="NONE",
-                              splunkbase_password="NONE",
-                              app_build_path="NONE",
-                              app_utilities="",
-                              my_github_token="NONE",
-                              current_branch="NONE",
-                              logger_log_files_prefix="NONE",
-                              logger_sourcetype="NONE"):
-
-        app_dir_path = os.path.join(os.path.dirname(__file__), "test_app_repos", test_app_repo)
-        print(f"TestIntegration.setup_action_yml_work -> app_dir_path={app_dir_path}")
-
-        # copy the app module there
-        shutil.copytree(app_dir_path, os.path.join("repodir"))
-
-        # setup inputs
-        os.environ["SPLUNK_app_dir"] = app_dir
-        os.environ["SPLUNK_is_generate_build"] = is_generate_build
-        os.environ["SPLUNK_use_ucc_gen"] = use_ucc_gen
-        os.environ["SPLUNK_to_make_permission_changes"] = to_make_permission_changes
-        os.environ["SPLUNK_is_app_inspect_check"] = is_app_inspect_check
-        os.environ["SPLUNK_splunkbase_username"] = splunkbase_username
-        os.environ["SPLUNK_splunkbase_password"] = splunkbase_password
-        os.environ["SPLUNK_app_build_path"] = app_build_path
-        os.environ["SPLUNK_app_utilities"] = app_utilities
-        os.environ["GITHUB_TOKEN"] = my_github_token
-        os.environ["SPLUNK_current_branch_name"] = current_branch
-        os.environ["SPLUNK_logger_log_files_prefix"] = logger_log_files_prefix
-        os.environ["SPLUNK_logger_sourcetype"] = logger_sourcetype
-
-
-    def teardown_method(self, test_method):
-        print("TestIntegration.teardown_method")
-        try:
-            shutil.rmtree("repodir")
-        except:
-            pass
-        
-        try:
-            shutil.rmtree("without_ucc_build")
-        except:
-            pass
-
-        try:
-            shutil.rmtree("with_ucc_build")
-        except:
-            pass
-
-        try:
-            shutil.rmtree("my_app_*")
-        except:
-            pass
 
 
     def extract_app_build(self, tgz_file):
@@ -143,12 +147,31 @@ class TestIntegration(unittest.TestCase):
 
 
 
-    def test_build_1(self):
-        self.setup_action_yml_work("repo_1_regular_build", app_dir="my_app_1", is_app_inspect_check="false")
-        main()
-        shutil.rmtree("my_app_1")
-        file_count, folder_count, all_files = self.extract_app_build("my_app_1_1_1_2_1.tgz")
-        assert folder_count == 7
-        assert file_count == 9
-        assert "my_app_1/static/appIconAlt.png" in all_files
-        assert "my_app_1/default/data/ui/views/assets.xml" in all_files
+    def test_build_1_regular(self):
+        with setup_action_yml_work("repo_1_regular_build", app_dir="my_app_1", is_app_inspect_check="false"):
+            main()
+
+            app_build_name = "my_app_1_1_1_2_1.tgz"
+            assert os.path.isfile(app_build_name)
+
+            file_count, folder_count, all_files = self.extract_app_build(app_build_name)
+            assert folder_count == 7
+            assert file_count == 10
+            assert "my_app_1/static/appIconAlt.png" in all_files
+            assert "my_app_1/default/data/ui/views/assets.xml" in all_files
+            assert "my_app_1/default/app.conf" in all_files
+
+
+    def test_build_repo_root_as_app_dir(self):
+        with setup_action_yml_work("repo_root_as_app_dir", app_dir=".", is_app_inspect_check="false"):
+            main()
+
+            app_build_name = "my_app_1_1_1_2_1.tgz"
+            assert os.path.isfile(app_build_name)
+
+            file_count, folder_count, all_files = self.extract_app_build(app_build_name)
+            assert folder_count == 7
+            assert file_count == 10
+            assert "my_app_1/static/appIconAlt.png" in all_files
+            assert "my_app_1/default/data/ui/views/assets.xml" in all_files
+            assert "my_app_1/default/app.conf" in all_files
