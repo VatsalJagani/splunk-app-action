538f704da6d831549817141cbeed4f8caeda4d69
Vatsal Jagani <vatsaljagani85@gmail.com>
2024-03-15 11:41:47 +0530

    splunk_app_action_ec0405c5aef93e771cd80e0db180b88b

--- tests/test_utilities_base.py
+++ tests/test_utilities_base.py
@@ -38,12 +38,12 @@ class TestBaseUtility(unittest.TestCase):
                 output = stdout.getvalue()
                 assert "Committing and creating PR for the code change." in output
 
-    # @patch('helpers.git_manager.get_multi_files_hash', return_value='hash')
-    # @patch('helpers.git_manager.GitHubPR')
-    # def test_add_multi_files_changed(self, mock_github_pr, mock_get_multi_files_hash):
-    #     with patch.object(self.base_utility, 'implement_utility', return_value=['/path/to/updated_file1.txt', '/path/to/updated_file2.txt']):
-    #         self.base_utility.add()
-    #         mock_github_pr.commit_and_pr.assert_called_once_with(hash='hash')
+    @patch('helpers.git_manager.get_multi_files_hash', return_value='hash')
+    @patch('helpers.git_manager.GitHubPR')
+    def test_add_multi_files_changed(self, mock_github_pr, mock_get_multi_files_hash):
+        with patch.object(self.base_utility, 'implement_utility', return_value=['/path/to/updated_file1.txt', '/path/to/updated_file2.txt']):
+            self.base_utility.add()
+            mock_github_pr.commit_and_pr.assert_called_once_with(hash='hash')
 
     # @patch('helpers.git_manager.GitHubPR')
     # def test_add_invalid_file(self, mock_github_pr):
