2809929f3c1802ac5cf97c873ef3d67aa2467617
VatsalJagani <vatsaljagani85@gmail.com>
2025-11-12 14:47:28 +0530

    Fix Read the Docs 404 - add root_doc to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,9 @@ release = VERSION
 
 # -- General configuration ---------------------------------------------------
 
+# The master document (root document for the documentation)
+root_doc = "index"
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
